a7d71e93adf84dbb5ba4e19175dbf222ae90be6e
GScarano97 <g.scarano@2610.it>
2026-01-14 15:26:42 +0100

    fix env

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyADbyz-xn47c7emLyg2Mza9e7T5a0cILFc",
+    authDomain: "ingrressiscientifica.firebaseapp.com",
+    projectId: "ingrressiscientifica",
+    storageBucket: "ingrressiscientifica.firebasestorage.app",
+    messagingSenderId: "283037814528",
+    appId: "1:283037814528:web:c2979e2dfc0d31a71eade5",
+    measurementId: "G-EW1RE82XN4",
+    region: "europe-west1"
+  }
+};
